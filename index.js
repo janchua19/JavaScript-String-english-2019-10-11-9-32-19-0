50e7a7176bfd30e0bd21c4a5144cf12fe1ed3cda
janchua19 <87625979+janchua19@users.noreply.github.com>
2021-07-22 15:11:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+console.log("'"+name.toUpperCase()+"'");
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+sentence = sentence.replace(/(\w)(\w*)/g , function(g0,g1,g2){return g1.toUpperCase() + g2.toLowerCase();});
+console.log(sentence);
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+console.log(money.slice(1,3));
